4ba38780d2be559b08f292101daab6a6f2a24dc9
shestakov.pe <21011975mail@gmail.com>
2024-04-23 22:08:22 +0300

    working trivial version of user

---
+++ user_interface.py
@@ -0,0 +1,17 @@
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+TEXT_AGREE = "Это сервис для напоминаниях о дедлайнах официальных мероприятиях. Хотите получать уведомления?"
+TEXT_UNSUBSCRIBE = "Хотите отписаться?"
+AGREEMENT = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='✅ Принимаю',
+                          callback_data='agree')],
+    [InlineKeyboardButton(text='❌ Отклоняю',
+                          callback_data='disagree')],
+])
+
+NEGATION = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='❌ Отписаться',
+                          callback_data='unsubscribe')],
+])
